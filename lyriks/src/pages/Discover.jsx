c4ce0c26d24f16fb62fc6686d8df4c703caf04b5
othea32 <othea32@gmail.com>
2022-11-13 16:53:00 -0800

    add top chart, top artist

--- lyriks/src/pages/Discover.jsx
+++ lyriks/src/pages/Discover.jsx
@@ -18,6 +18,7 @@ const Discover = () => {
   <div className="flex flex-col">
    <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
     <h2 className="font-bold text-3x1 text-white text-left">Discover {genreTitle}</h2>
+    
     <select
      onChange={() => {}}
      value=''
